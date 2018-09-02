f2203ec64bbc9ed54feb988701434d720c9d371a
Vijay Narayan <vijay.narayan@live.com>
2018-09-02 23:12:04 +0530

    Added Array problems

---
+++ src/Algos/Array/ArrayProblems.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algos.Array
+{
+    public class ArrayProblems
+    {
+        /// <summary>
+        /// Write a function rotate(ar[], d) that rotates arr[] by x elements.
+        /// Using temp array.
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="x"></param>
+        public static void RotateArrayByxElements(int[] arr, int x)
+        {
+            // If rotation number is > array length then find the modulus and only rotate by that number.
+            if (x > arr.Length)
+            {
+                x = x % arr.Length;
+            }
+
+            int[] tempArray = new int[x];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                tempArray[i] = arr[arr.Length - 1 - i];
+            }
+
+            for (int i = arr.Length - 1 - x; i >= 0; i--)
+            {
+                arr[i + x] = arr[i];
+            }
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                arr[x - i - 1] = tempArray[i];
+            }
+        }
+
+        /// <summary>
+        /// Given an array A[] and a number x, check for pair in A[] with sum as x.
+        /// </summary>
+        /// <returns></returns>
+        public static bool FindIfPairExistForSumX(int[] arr, int d)
+        {
+
+            return true;
+        }
+    }
+}
---
+++ src/UnitTest/ArrayTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Algos;
+using Algos.Array;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ArrayTests
+    {
+        //private int[] arr = { 40, 10, 80, 30, 90, 50, 70, 76, 57, 81, 16, 39, -23, -908, -1 };
+        private int[] arr = { 30, 40, 50, 60, 70, 80, 90, 100, 200, 300};
+
+        [TestMethod]
+        public void Test_LinearSearch()
+        {
+            ArrayProblems.Rotate(arr, 15);
+        }
+    }
+}
